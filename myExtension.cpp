a88046384ca07b4830ba413001d659d8fe5282d9
atsushi-morita <himarayayukiotoko@gmail.com>
2023-07-25 15:35:28 +0900

    aaa

--- myExtension.cpp
+++ myExtension.cpp
@@ -1,22 +1,23 @@
 #include "pxt.h"
 using namespace pxt;
 
-//% block
-void myFunction(StringData* text) {
-    int len = text->getLength();
-    const char16_t* data = text->getData();
+namespace myNamespace {
+    void myFunction(StringData* text) {
+        int len = text->getLength();
+        const char16_t* data = text->getData();
 
-    // iterate over each char16_t element
-    for(int i = 0; i < len; ++i) {
-        // you can access each char16_t element as data[i]
-        uBit.serial.printf("%04x ", data[i]);
+        // iterate over each char16_t element
+        for(int i = 0; i < len; ++i) {
+            // you can access each char16_t element as data[i]
+            uBit.serial.printf("%04x ", data[i]);
+        }
+        uBit.serial.printf("\n");
     }
-    uBit.serial.printf("\n");
 }
 
 namespace myExtension {
-    //% shim=myExtension::myFunction
+    //% shim=myNamespace::myFunction
     void myFunction(String text) {
-        myFunction(text->getUTF16Data());
+        myNamespace::myFunction(text->getUTF16Data());
     }
 }
